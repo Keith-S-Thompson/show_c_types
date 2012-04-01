a31586505136c49abd6ce5d3bc03011e9188467b
Keith Thompson <Keith.S.Thompson@gmail.com>
2012-04-01 16:46:52 -0700

    Add copyright notices

--- show_c_types.c
+++ show_c_types.c
@@ -1,4 +1,8 @@
 /*
+ * Copyright (C) 2012 Keith Thompson
+ */
+
+/*
  * This file is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
