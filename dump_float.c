460977ece31e6bff7025554656cc6a8ddfa88367
Keith Thompson <Keith.S.Thompson@gmail.com>
2012-11-28 13:28:15 -0800

    Add a fractional value

--- dump_float.c
+++ dump_float.c
@@ -27,30 +27,39 @@ static char *hex_image(const void *base, size_t size) {
     return result;
 } /* hex_image */
 
-#define SHOW(type, name, value) printf("%-12s %6s  %s\n", type, name, hex_image(&value, sizeof value))
+#define SHOW(type, name, value) printf("%-12s %7s  %s\n", type, name, hex_image(&value, sizeof value))
 
 int main(void) {
-
     const static float       f_one            =   1.0F;
+    const static float       f_fraction       = 1.09375F;
     const static float       f_minus_sixteen  = -16.0F;
     const static float       f_one_million    = 1.0e6F;
+
     const static double      d_one            =   1.0;
+    const static double      d_fraction       = 1.09375;
     const static double      d_minus_sixteen  = -16.0;
     const static double      d_one_million    = 1.0e6;
+
 #ifndef DISABLE_LONG_DOUBLE
     const static long double ld_one           =   1.0L;
+    const static long double ld_fraction      = 1.09375L;
     const static long double ld_minus_sixteen = -16.0L;
     const static long double ld_one_million   = 1.0e6L;
 #endif
 
     SHOW("float", "1.0", f_one);
+    SHOW("float", "1.09375", f_fraction);
     SHOW("float", "-16.0", f_minus_sixteen);
     SHOW("float", "1.0e6", f_one_million);
+
     SHOW("double", "1.0", d_one);
+    SHOW("double", "1.09375", d_fraction);
     SHOW("double", "-16.0", d_minus_sixteen);
     SHOW("double", "1.0e6", d_one_million);
+
 #ifndef DISABLE_LONG_DOUBLE
     SHOW("long double", "1.0", ld_one);
+    SHOW("long double", "1.09375", ld_fraction);
     SHOW("long double", "-16.0", ld_minus_sixteen);
     SHOW("long double", "1.0e6", ld_one_million);
 #endif
